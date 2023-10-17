077599b905951d883b283d078e2f8c74a1074782
Adrian Pete Lagare <aprlagare1999@gmail.com>
2023-10-17 16:52:23 +0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,16 @@ data_files.append(
     ("share/ament_index/resource_index/packages", ["resource/" + package_name])
 )
 data_files.append(("share/" + package_name, ["package.xml"]))
+
+data_files.append(
+    (
+        "share/" + package_name + "/worlds/GearsNav2World/worlds",
+        ["worlds/GearsNav2World/worlds/GearsNav2World.wbt"],
+    )
+)
+data_files.append(("share/" + package_name + "/launch", ["launch/robot_launch.py"]))
+
+
 setup(
     name=package_name,
     version="0.0.0",
