e868a0ea81cc744a0b080cb39059688b5acc40ea
Adrian Pete Lagare <aprlagare1999@gmail.com>
2023-10-17 17:04:49 +0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -20,15 +20,18 @@ data_files.append(
 )
 data_files.append(("share/" + package_name + "/launch", ["launch/robot_launch.py"]))
 
+data_files.append(
+    (
+        "share/" + package_name + "/worlds/GearsNav2World/protos",
+        ["worlds/GearsNav2World/protos/GearsRover.proto"],
+    )
+)
 
 setup(
     name=package_name,
     version="0.0.0",
     packages=find_packages(exclude=["test"]),
-    data_files=[
-        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
-        ("share/" + package_name, ["package.xml"]),
-    ],
+    data_files=data_files,
     install_requires=["setuptools"],
     zip_safe=True,
     maintainer="minegearscsu",
