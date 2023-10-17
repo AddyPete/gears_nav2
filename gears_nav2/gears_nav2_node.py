91ae961a0a2f40bd58d79d3670cd146f51b71c60
Adrian Pete Lagare <aprlagare1999@gmail.com>
2023-10-17 16:47:35 +0800

    1st Commit

---
+++ gears_nav2/gears_nav2_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from gears_nav2.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = "gears_nav2"
+data_files = []
+
+data_files.append(
+    ("share/ament_index/resource_index/packages", ["resource/" + package_name])
+)
+data_files.append(("share/" + package_name, ["package.xml"]))
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="minegearscsu",
+    maintainer_email="minegearscsu@todo.todo",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": ["gears_nav2_node = gears_nav2.gears_nav2_node:main"],
+    },
+)
