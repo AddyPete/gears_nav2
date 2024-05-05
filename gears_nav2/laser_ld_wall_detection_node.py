935bc72bcc25dec1d17a8be9fd5b2ce85199b8f8
Adrian Pete Lagare <aprlagare1999@gmail.com>
2024-05-06 02:05:29 +0800

    updated lidar node

--- gears_nav2/laser_ld_wall_detection_node.py
+++ gears_nav2/laser_ld_wall_detection_node.py
@@ -4,8 +4,11 @@ from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Bool
 
-LIDAR_RANGE_THRESHOLD = 1.4  # meters to turn
+LIDAR_RANGE_THRESHOLD = 0.8  # meters to turn
 LIDAR_POINTS_THRESHOLD = 30  # points
+
+LIDAR_RANGE_THRESHOLD_SIDE = 1.5
+LIDAR_POINTS_THRESHOLD_SIDE = 20
 
 
 class LaserLDWallDetection(Node):
@@ -19,13 +22,26 @@ class LaserLDWallDetection(Node):
         )
         self._lidar_points = np.empty([])
         self._wall_collision_publisher = self.create_publisher(
-            Bool, "/is_near_wall", 10
+            Bool, "/lidar/is_near_front_wall", 10
         )
 
         self.__is_near_wall = False
+        self.__is_near_left_wall = False
+        self.__is_near_right_wall = False
+
+        self._wall_left_collision_publisher = self.create_publisher(
+            Bool, "/lidar/is_near_left_wall", 10
+        )
+
+        self._wall_right_collision_publisher = self.create_publisher(
+            Bool, "/lidar/is_near_right_wall", 10
+        )
+
         timer_period = 1.0 / 20  # seconds (20Hz)
         self.timer = self.create_timer(timer_period, self.wall_detect_timer_callback)
         self.is_near_wall_msg = Bool()
+        self.is_near_wall_msg_left_msg = Bool()
+        self.is_near_wall_msg_right_msg = Bool()
 
     def laser_callback(self, lidar_msg):
         lidar_points = np.array(lidar_msg.ranges)
@@ -33,12 +49,19 @@ class LaserLDWallDetection(Node):
         self._lidar_points = lidar_points
 
         self.wall_detector()
+        self.side_wall_detector()
 
     def wall_detect_timer_callback(self):
 
         self.is_near_wall_msg.data = self.__is_near_wall
 
         self._wall_collision_publisher.publish(self.is_near_wall_msg)
+
+        self.is_near_wall_msg_left_msg.data = self.__is_near_left_wall
+        self._wall_left_collision_publisher.publish(self.is_near_wall_msg_left_msg)
+
+        self.is_near_wall_msg_right_msg.data = self.__is_near_right_wall
+        self._wall_right_collision_publisher.publish(self.is_near_wall_msg_right_msg)
 
     def wall_detector(self):
 
@@ -57,9 +80,9 @@ class LaserLDWallDetection(Node):
             self._lidar_obstacle_count = np.sum(
                 lidar_front_view <= LIDAR_RANGE_THRESHOLD
             )
-            self.get_logger().info(
-                f"Lidar Obstacle Points: {self._lidar_obstacle_count}"
-            )
+            # self.get_logger().info(
+            #     f"Lidar Obstacle Points: {self._lidar_obstacle_count}"
+            # )
 
             if self._lidar_obstacle_count >= LIDAR_POINTS_THRESHOLD:
 
@@ -68,6 +91,45 @@ class LaserLDWallDetection(Node):
                 self.__is_near_wall = False
         except IndexError as e:
             self.get_logger().error(f"IndexError in need_to_rotate: {e}")
+        except Exception as e:
+            self.get_logger().error(f"Unexpected error in need_to_rotate: {e}")
+
+    def side_wall_detector(self):
+        try:
+            lidar_points = self._lidar_points
+
+            lidar_left_side = np.array(lidar_points[0 : self.scale_lidar(44)])
+            lidar_right_side = np.array(
+                lidar_points[self.scale_lidar(314) : self.scale_lidar(359)]
+            )
+
+            self._lidar_obstacle_count_left = np.sum(
+                lidar_left_side <= LIDAR_RANGE_THRESHOLD_SIDE
+            )
+            self._lidar_obstacle_count_right = np.sum(
+                lidar_right_side <= LIDAR_RANGE_THRESHOLD_SIDE
+            )
+
+            self.get_logger().info(
+                f"Lidar Obstacle Points Left: {self._lidar_obstacle_count_left}"
+            )
+
+            self.get_logger().info(
+                f"Lidar Obstacle Points Right: {self._lidar_obstacle_count_right}"
+            )
+
+            if self._lidar_obstacle_count_left >= LIDAR_POINTS_THRESHOLD_SIDE:
+
+                self.__is_near_left_wall = True
+            else:
+                self.__is_near_left_wall = False
+
+            if self._lidar_obstacle_count_right >= LIDAR_POINTS_THRESHOLD_SIDE:
+
+                self.__is_near_right_wall = True
+            else:
+                self.__is_near_right_wall = False
+
         except Exception as e:
             self.get_logger().error(f"Unexpected error in need_to_rotate: {e}")
 
